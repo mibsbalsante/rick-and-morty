8b526d49fff04bcf5a9534cfc6e2fac9bdef6ac2
Melissa Balsante <mibsbalsante@gmail.com>
2023-03-08 02:31:58 -0300

    feat(data): filter and paginate gql results

--- src/components/HomeList.vue
+++ src/components/HomeList.vue
@@ -1,33 +1,57 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue'
-import gql from 'graphql-tag'
 import { useQuery } from '@vue/apollo-composable'
 
-const CHARACTERS_QUERY = gql`
-  query Characters($page: Int) {
-    characters(page: $page) {
-      results {
-        id
-        name
-        image
-      }
-    }
-  }
-`
+import { CHARACTERS_QUERY } from '@gql/index'
+
+const PAGE_RESULTS = 20
 
 const pageNumber = ref(1)
-const queryConfig = computed(() => ({ page: pageNumber.value }))
+const filterName = ref('')
+const queryConfig = computed(() => ({
+  page: pageNumber.value,
+  name: filterName.value
+}))
 
 const data = useQuery(CHARACTERS_QUERY, queryConfig)
 
 const characters = computed(() => data?.result?.value?.characters?.results)
+const charactersCount = computed(
+  () => data?.result?.value?.characters?.info?.count || 0
+)
+
+const hasNextPage = computed(
+  () => Math.ceil(charactersCount.value / PAGE_RESULTS) > pageNumber.value
+)
 </script>
 
 <template>
-  <ul>
-    <li v-for="character in characters" :key="character.id">
-      <p>{{ character.name }}</p>
-      <img :src="character.image" />
-    </li>
-  </ul>
+  <div>
+    <q-input
+      outlined
+      v-model="filterName"
+      debounce="500"
+      label="Search by Name"
+    />
+
+    <ul>
+      <li v-for="character in characters" :key="character.id">
+        <p>{{ character.name }}</p>
+        <q-img :src="character.image" :ratio="1" loading="lazy" width="300px" />
+      </li>
+    </ul>
+    <p>Results: {{ charactersCount || 'Unknown' }}</p>
+    <q-btn
+      :disabled="pageNumber <= 1"
+      color="primary"
+      label="Last Page"
+      @click="() => (pageNumber = pageNumber - 1)"
+    />
+    <q-btn
+      :disabled="!hasNextPage"
+      color="primary"
+      label="Next Page"
+      @click="() => (pageNumber = pageNumber + 1)"
+    />
+  </div>
 </template>
