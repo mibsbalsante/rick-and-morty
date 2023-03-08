fc94a625e7ec2b24acc4683395b79a2aca74a388
Melissa Balsante <mibsbalsante@gmail.com>
2023-03-08 02:07:49 -0300

    refactor(queries): move queries outside of components

---
+++ src/queries/index.ts
@@ -0,0 +1,43 @@
+import gql from 'graphql-tag'
+
+export const CHARACTERS_QUERY = gql`
+  query Characters($page: Int, $name: String) {
+    characters(page: $page, filter: { name: $name }) {
+      info {
+        count
+      }
+      results {
+        id
+        name
+        image
+      }
+    }
+  }
+`
+
+export const CHARACTER_QUERY = gql`
+  query Character($id: Int) {
+    character(id: $id) {
+      name
+      species
+      status
+      origin {
+        name
+      }
+      location {
+        name
+      }
+      episode {
+        id
+        name
+        episode
+        air_date
+        characters {
+          id
+          name
+          image
+        }
+      }
+    }
+  }
+`
--- vite.config.ts
+++ vite.config.ts
@@ -28,6 +28,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
       '@ast': fileURLToPath(new URL('./src/assets', import.meta.url)),
       '@cmp': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@gql': fileURLToPath(new URL('./src/queries', import.meta.url)),
       '@utl': fileURLToPath(new URL('./src/utils', import.meta.url))
     }
   },
